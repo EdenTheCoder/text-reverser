2c91c312e05054916a965922652456d50836450d
EdenTheCoder <Edenmarks0812@gmail.com>
2024-03-17 15:22:30 +0200

    rtl support

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let diraction;
+diraction = "ltr";
+
 function strRevese(toRevese) {
 	console.log("HI");
 	return toRevese.split("").reverse().join("");
@@ -18,4 +21,15 @@ document.getElementById("copyToClipBoard").addEventListener("click", function ()
 	navigator.clipboard.writeText(document.getElementById("outputText").innerHTML).then(function () {
 		alert("copied text : " + document.getElementById("outputText").innerHTML);
 	});
+});
+document.getElementById("diraction button").addEventListener("click", function () {
+	if (diraction == "ltr") {
+		diraction = "rtl";
+		document.getElementById("fInput").style.direction = "rtl";
+		document.getElementById("diraction button").innerText = "dircation : rtl";
+	} else {
+		diraction = "ltr";
+		document.getElementById("fInput").style.direction = "ltr";
+		document.getElementById("diraction button").innerText = "dircation : ltr";
+	}
 });
